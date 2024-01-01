32916ea83212d75930c78b927a21d04a12af59e4
Yash09042004 <yashkiran2004@gmail.com>
2024-01-01 10:27:31 +0530

    Added Git, OpenVPN and Docker

--- Scripts/Installer.sh
+++ Scripts/Installer.sh
@@ -25,7 +25,10 @@ echo " 1)Visual Studio Code
       14)Spotify
       15)Blender
       16)Audacity
-      17)Lollypop Music Player"
+      17)Lollypop Music Player
+      18)Git
+      19)Telegram
+      20)Docker"
 read OPTION
 
 # echo -n "The official language of $COUNTRY is "
@@ -180,6 +183,33 @@ case $OPTION in
    sleep 1
    clear
   ;;
+  18)
+   echo -n "Installing Git"
+   sudo apt update > file.txt 2>&1
+   sudo apt install git -y > file.txt 2>&1
+   echo -n "Git installed successfully"
+   sleep 1
+   clear
+  ;;
+  19)
+   echo -n "Installing OpenVPN"
+   sudo apt update > file.txt 2>&1
+   sudo apt-get install openvpn -y > file.txt 2>&1
+   echo -n "OpenVPN installed successfully"
+   sleep 1
+   clear
+  ;;
+  20)
+   echo -n "Installing Docker"
+   sudo apt update > file.txt 2>&1
+   sudo apt-get install curl  -y> file.txt 2>&1
+   curl -fsSL https://get.docker.com/ | sh > file.txt 2>&1
+   sudo usermod -aG docker $(whoami) > file.txt 2>&1
+   echo -n "Docker installed successfully"
+   sleep 1
+   clear
+  ;;
+
   
     
 
